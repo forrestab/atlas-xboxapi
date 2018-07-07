814539b99275d0b20a256964f662efebd8dc79c8
Forrest Brown <forrestabrown@gmail.com>
2018-07-07 00:05:37 -0400

    test: add test for `GetXuid`

---
+++ test/Atlas.Integration.Xbox.Test/XboxClientTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Atlas.Integration.Xbox.Test
+{
+    public class XboxClientTests
+    {
+        [Theory]
+        [InlineData("desugoji", 1)]
+        public async Task GetXuid_ReturnsXuid(string gamertag, long expected)
+        {
+            Mock<IXboxClient> Client = new Mock<IXboxClient>();
+
+            Client
+                .Setup(s => s.GetXuid(gamertag))
+                .Returns(Task.FromResult(expected));
+
+            Assert.Equal(expected, await Client.Object.GetXuid(gamertag));
+        }
+    }
+}
